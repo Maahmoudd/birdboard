6993fc9f96e20c3f88ad1ce853e6e0bf438a8dcb
Maahmoudd <mahmoudmuhammed2610@gmail.com>
2024-01-07 21:50:48 +0200

    Testing Projects Creation

---
+++ resources/views/index.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <h1>BirdBoard</h1>
+
+    <ul>
+        @foreach ($projects as $project)
+            <li>{{ $project->title }}</li>
+        @endforeach
+    </ul>
+</body>
+</html>
